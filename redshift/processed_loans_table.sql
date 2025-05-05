0b3754367b825af4623f8f5a79714088a3ed9f8b
bakliwalvaibhav1 <vbakliwalcodes@gmail.com>
2025-05-04 22:44:21 -0500

    chore: add sql table for processed loans

---
+++ redshift/processed_loans_table.sql
@@ -0,0 +1,18 @@
+CREATE TABLE processed_loans (
+    loan_amnt FLOAT,
+    term VARCHAR(20),
+    int_rate FLOAT,
+    installment FLOAT,
+    grade VARCHAR(5),
+    emp_length VARCHAR(20),
+    home_ownership VARCHAR(20),
+    annual_inc FLOAT,
+    purpose VARCHAR(50),
+    addr_state VARCHAR(10),
+    dti FLOAT,
+    delinq_2yrs FLOAT,
+    revol_util FLOAT,
+    total_acc FLOAT,
+    loan_status VARCHAR(30),
+    issue_d DATE
+);
